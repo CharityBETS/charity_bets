f762de15342a03959400c6907cd18835bf969df8
Bret Runestad <bret.runestad@gmail.com>
2015-03-16 09:31:06 -0400

    running on manage.py

--- charity_bets/__init__.py
+++ charity_bets/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 
 from . import models
-from .extensions import db, migrate, config
-from .views import coaction
-
+from .extensions import db, migrate, config, oauth, assets
+# from .views.views import coaction
+from .views.home import home
+from .views.users import users
 
 SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/coaction.db"
 DEBUG = True
 SECRET_KEY = 'development-key'
+FACEBOOK={'consumer_key': '1417359091908563','consumer_secret': 'f129f7b34d7b57201da77f5ea0432a45'}
 
 
 def create_app():
@@ -17,6 +19,9 @@ def create_app():
     app.register_blueprint(users)
 
     config.init_app(app)
+    oauth.init_app(app)
+    assets.init_app(app)
+
     db.init_app(app)
     migrate.init_app(app, db)
 
