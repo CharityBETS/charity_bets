c71d8ca7e55d70e6386b9244854af8897bbdc99b
hifilorau <tomrau@gmail.com>
2015-03-16 10:26:39 -0400

    skeletons html for create bet and landing

---
+++ src/landing/landing-controller.js
@@ -0,0 +1,15 @@
+app.config(['$routeProvider', function($routeProvider) {
+  var routeDefinition = {
+    templateUrl: 'static/landing/landing.html',
+    controller: 'LandingCtrl',
+    controllerAs: 'vm',
+  };
+  $routeProvider.when('/', routeDefinition);
+}])
+.controller('LandingCtrl', ['$location', function ($location) {
+
+  var self = this;
+
+
+
+}]);
