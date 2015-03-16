6137ea01d2edf236b77ced0d58584d626d783db3
hifilorau <tomrau@gmail.com>
2015-03-16 18:08:47 -0400

    checkin

---
+++ src/bet-view/bet.js
@@ -0,0 +1,13 @@
+app.factory('Bet', function () {
+  return function (spec) {
+    spec = spec || {};
+    return {
+        title: spec.title,
+        challenger: spec.challenger,
+        amount: spec.amount,
+        date: spec.date,
+        location: spec.location,
+        description: spec.description
+    };
+  };
+});
