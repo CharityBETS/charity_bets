d44576b1162758db15529fd3c9cc06c60cd02bf4
Bret Runestad <bret.runestad@gmail.com>
2015-03-17 10:57:17 -0400

    added to requirements.txt folder

--- charity_bets/__init__.py
+++ charity_bets/__init__.py
@@ -12,8 +12,8 @@ MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = "xxxx@gmail.com"
-MAIL_PASSWORD = "xxxx"
+MAIL_USERNAME = "betsforcharity@gmail.com"
+MAIL_PASSWORD = "noblebookie"
 
 
 def create_app():
