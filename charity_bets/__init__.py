95a1922cd6a669109a3ba530df6b219f11c7ac2c
Bret Runestad <bret.runestad@gmail.com>
2015-03-17 10:38:16 -0400

    adding mail setup in extensions and init

--- charity_bets/__init__.py
+++ charity_bets/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
 from . import models
-from .extensions import db, migrate, config, oauth, assets, login_manager
+from .extensions import db, migrate, config, oauth, assets, login_manager, mail
 # from .views.views import coaction
 from .views.home import home
 from .views.users import users
 from .views.bets import bets
+
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = "xxxx@gmail.com"
+MAIL_PASSWORD = "xxxx"
 
 
 def create_app():
@@ -16,6 +24,7 @@ def create_app():
     app.config['SECRET_KEY']
     app.config['FACEBOOK']
 
+
     app.register_blueprint(home)
     app.register_blueprint(users)
     app.register_blueprint(bets, url_prefix='/api')
@@ -23,6 +32,7 @@ def create_app():
     config.init_app(app)
     oauth.init_app(app)
     assets.init_app(app)
+    mail.init_app(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
