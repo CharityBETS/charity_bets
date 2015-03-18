622234444b03914a7372acf6cfa45eaf44fbf635
hifilorau <tomrau@gmail.com>
2015-03-18 05:08:29 -0400

    profile-page

--- charity_bets/static/js/app.js
+++ charity_bets/static/js/app.js
@@ -97,6 +97,60 @@ app.config(['$routeProvider', function($routeProvider) {
 
 }]);
 
+app.directive('textarea', function() {
+    return {
+        restrict: 'E',
+        link: function( scope , element , attributes ) {
+            var threshold    = 35,
+                minHeight    = element[0].offsetHeight,
+                paddingLeft  = element.css('paddingLeft'),
+                paddingRight = element.css('paddingRight');
+
+            var $shadow = angular.element('<div></div>').css({
+                position:   'absolute',
+                top:        -10000,
+                left:       -10000,
+                width:      element[0].offsetWidth - parseInt(paddingLeft || 0) - parseInt(paddingRight || 0),
+                fontSize:   element.css('fontSize'),
+                fontFamily: element.css('fontFamily'),
+                lineHeight: element.css('lineHeight'),
+                resize:     'none'
+            });
+
+            angular.element( document.body ).append( $shadow );
+
+            var update = function() {
+                var times = function(string, number) {
+                    for (var i = 0, r = ''; i < number; i++) {
+                        r += string;
+                    }
+                    return r;
+                }
+
+                var val = element.val().replace(/</g, '&lt;')
+                    .replace(/>/g, '&gt;')
+                    .replace(/&/g, '&amp;')
+                    .replace(/\n$/, '<br/>&nbsp;')
+                    .replace(/\n/g, '<br/>')
+                    .replace(/\s{2,}/g, function( space ) {
+                        return times('&nbsp;', space.length - 1) + ' ';
+                    });
+
+                $shadow.html( val );
+
+                element.css( 'height' , Math.max( $shadow[0].offsetHeight + threshold , minHeight ) );
+            }
+
+            scope.$on('$destroy', function() {
+                $shadow.remove();
+            });
+
+            element.bind( 'keyup keydown keypress change' , update );
+            update();
+        }
+    }
+});
+
 app.config(['$routeProvider', function($routeProvider) {
   var routeDefinition = {
     templateUrl: 'static/bets/bets.html',
@@ -249,16 +303,19 @@ app.factory('userService', ['$http', '$q', '$log', function($http, $q, $log) {
 
 app.config(['$routeProvider', function($routeProvider) {
   var routeDefinition = {
-    templateUrl: 'static/user/user.html',
+    templateUrl: 'static/user/user-profile.html',
     controller: 'UserCtrl',
-    controllerAs: 'vm'
-    // resolve: {
-    //   currentUser: ['userService', function (userService){
-    //     return userService.getCurrent();
-    //   }]
-    //   }
+    controllerAs: 'vm',
+    resolve: {
+          currentUser: ['userService', function (userService) {
+          console.log(userService.getCurrent());
+          return userService.getCurrent().then(function (result) {
+            return result.data;
+          });
+        }]
+      }
   };
-  $routeProvider.when('/users', routeDefinition);
+  $routeProvider.when('/user/user-profile', routeDefinition);
 }])
 .controller('UserCtrl', ['$location', 'userService', 'currentUser', function ($location, userService, currentUser) {
 
