bcb8acb282e195ca3774815367b37e2a031ff3d9
Bret Runestad <bret.runestad@gmail.com>
2015-03-18 16:31:30 -0400

    trying threading, not working yet

--- charity_bets/emails.py
+++ charity_bets/emails.py
@@ -1,12 +1,21 @@
 from flask.ext.mail import Message
 from charity_bets import mail
-from flask import render_template
+from flask import render_template, current_app
+from threading import Thread
+
+
+def send_async_email(app, message):
+    with app.app_context():
+        mail.send(message)
 
 def send_email(subject, sender, recipients, text_body, html_body):
+    app = current_app._get_current_object()
     message = Message(subject, sender=sender, recipients=recipients)
     message.body = text_body
     message.html = html_body
-    mail.send(message)
+    thr = Thread(target=send_async_email, args=[app, message])
+    thr.start()
+    return thr
 
 def bet_creation_notification(creator, challenger, bet):
     send_email("You've been challenged to a bet!",
