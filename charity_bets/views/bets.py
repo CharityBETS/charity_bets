bc1b0c281745c54a8b2a397a7426feb2db202608
Bret Runestad <bret.runestad@gmail.com>
2015-03-19 17:17:10 -0400

    updated email switch, added site to email template

--- charity_bets/views/bets.py
+++ charity_bets/views/bets.py
@@ -8,6 +8,7 @@ from ..emails import send_email, bet_creation_notification
 import json
 from charity_bets import mail
 from flask_mail import Message
+from ..email_switch import emailing
 
 
 bets = Blueprint("bets", __name__)
@@ -52,8 +53,7 @@ def create_bet():
         db.session.commit()
 
         # Message sent to the other party of the bet
-        emailing is False:
-        if emailing:
+        if emailing == "on":
             bet_creation_notification(current_user, challenger, bet)
 
         bet = bet.make_dict()
