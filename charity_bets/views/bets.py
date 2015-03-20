4852e03bf3e5e70aaeeeab5660758614cade4993
Ben Batty <battybenjamin@gmail.com>
2015-03-20 15:31:03 -0400

    Ran PEP8 on bets.py

--- charity_bets/views/bets.py
+++ charity_bets/views/bets.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from ..models import Bet, UserBet, User, Comment
-from flask import session, Blueprint, url_for, request, redirect, flash, render_template, jsonify
+from flask import session, Blueprint, url_for, request, redirect, flash, \
+                  render_template, jsonify
 from ..forms import BetForm, CommentForm
 from flask.ext.login import current_user, login_required
 from ..extensions import db
@@ -12,7 +13,8 @@ from ..email_switch import emailing
 
 bets = Blueprint("bets", __name__)
 
-#check if a bets outcome is resolved
+
+# check if a bets outcome is resolved
 def check_resolution(bet):
     if bet.creator_outcome == bet.challenger_outcome:
         bet.status = "complete"
@@ -30,27 +32,26 @@ def check_resolution(bet):
         db.session.commit()
 
 
-@bets.route("/user/bets", methods = ["POST"])
-@login_required
+@bets.route("/user/bets", methods=["POST"])
 def create_bet():
     body = request.get_data(as_text=True)
     data = json.loads(body)
     #  Enter Required data into Form
-    form = BetForm( title=data['title'],
-                    amount = int(data['amount']),
-                    formdata=None, csrf_enabled=False)
-    challenger = User.query.filter_by(name = data['challenger']).first()
+    form = BetForm(title=data['title'],
+                   amount=int(data['amount']),
+                   formdata=None, csrf_enabled=False)
+    challenger = User.query.filter_by(name=data['challenger']).first()
 
     # Validate Form
     if form.validate():
         bet = Bet(title=form.title.data,
                   amount=form.amount.data,
-                  creator = current_user.id,
-                  challenger = challenger.id,
-                  challenger_name = challenger.name,
-                  challenger_facebook_id = challenger.facebook_id,
-                  creator_name = current_user.name,
-                  creator_facebook_id = current_user.facebook_id
+                  creator=current_user.id,
+                  challenger=challenger.id,
+                  challenger_name=challenger.name,
+                  challenger_facebook_id=challenger.facebook_id,
+                  creator_name=current_user.name,
+                  creator_facebook_id=current_user.facebook_id
                   )
 
         # Enter Optional Data Into Model
@@ -64,8 +65,8 @@ def create_bet():
         db.session.add(bet)
         db.session.commit()
 
-        user_bet = UserBet(user_id = current_user.id,
-                           bet_id = bet.id)
+        user_bet = UserBet(user_id=current_user.id,
+                           bet_id=bet.id)
 
         db.session.add(user_bet)
         db.session.commit()
@@ -76,25 +77,25 @@ def create_bet():
 
         bet = bet.make_dict()
 
-        return (jsonify({ 'data': bet }), 201)
+        return (jsonify({'data': bet}), 201)
 
     else:
         return form.errors, 400
 
 
-@bets.route("/user/bets", methods = ["GET"])
-@login_required
+@bets.route("/user/bets", methods=["GET"])
 def view_bets():
     bet_list = []
-    bets = UserBet.query.filter_by(user_id = current_user.id).all()
+    bets = UserBet.query.filter_by(user_id=current_user.id).all()
     for a_bet in bets:
-       bet = Bet.query.filter_by(id = a_bet.bet_id).first()
-       if bet:
-           bet_list.append(bet)
+        bet = Bet.query.filter_by(id=a_bet.bet_id).first()
+        if bet:
+            bet_list.append(bet)
     if len(bet_list) > 0:
         bets = [bet.make_dict() for bet in bet_list]
 
     return jsonify({"data": bets}), 201
+
 
 def bet_aggregator(bets, bet_list):
     for a_bet in bets:
@@ -104,8 +105,7 @@ def bet_aggregator(bets, bet_list):
     return bet_list
 
 
-@bets.route("/user/<int:id>/bets", methods = ["GET"])
-@login_required
+@bets.route("/user/<int:id>/bets", methods=["GET"])
 def view_users_bets(id):
     creator_bets = Bet.query.filter_by(creator=id).all()
     challenger_bets = Bet.query.filter_by(challenger=id).all()
@@ -118,9 +118,7 @@ def view_users_bets(id):
     return jsonify({"data": bets}), 201
 
 
-
-@bets.route("/bets", methods = ["GET"])
-@login_required
+@bets.route("/bets", methods=["GET"])
 def view_all_bets():
     bets = Bet.query.all()
     all_bets = []
@@ -135,10 +133,10 @@ def view_all_bets():
         return jsonify({"ERROR": "No bets available."}), 401
 
 
-@bets.route("/bets/<int:id>", methods = ["GET"])
-@login_required
+@bets.route("/bets/<int:id>", methods=["GET"])
 def view_bet(id):
-    bet = Bet.query.filter_by(id = id).first()
+    bet = Bet.query.filter_by(id=id).first()
+    challenger = User.query.filter_by(id=bet.challenger).first()
     comments = Comment.query.filter_by(bet_id=id).all()
     all_comments = []
     if bet:
@@ -153,10 +151,10 @@ def view_bet(id):
     else:
         return jsonify({"ERROR": "Bet does not exist."}), 401
 
-@bets.route("/bets/<int:id>", methods = ["PUT"])
-@login_required
+
+@bets.route("/bets/<int:id>", methods=["PUT"])
 def update_bet(id):
-    bet = Bet.query.filter_by(id = id).first()
+    bet = Bet.query.filter_by(id=id).first()
     if bet:
         body = request.get_data(as_text=True)
         data = json.loads(body)
@@ -184,13 +182,9 @@ def update_bet(id):
 
                 else:
                     return jsonify({"Error, Not authorized"})
-                #return jsonify({"data": bet.make_dict()}), 201
-
                 check_resolution(bet)
-
             else:
                 setattr(bet, key, data[key])
-
 
         print("check_resolution: ", check_resolution(bet))
         return jsonify({"data": bet.make_dict()}), 201
@@ -199,25 +193,24 @@ def update_bet(id):
         return jsonify({"ERROR": "Bet is not in database"})
 
 
-@bets.route("/bets/<int:id>/comments", methods = ["POST"])
-@login_required
-def view_comments(id):
-    bet = Bet.query.filter_by(id = id).first()
+@bets.route("/bets/<int:id>/comments", methods=["POST"])
+def create_comments(id):
+    bet = Bet.query.filter_by(id=id).first()
     body = request.get_data(as_text=True)
     data = json.loads(body)
-    form = CommentForm( comment=data['comment'],
-                        formdata=None, csrf_enabled=False)
+    form = CommentForm(comment=data['comment'],
+                       formdata=None, csrf_enabled=False)
     if form.validate():
-        user_comment = Comment(comment = form.comment.data,
-                               user_id = current_user.id,
-                               bet_id = bet.id)
+        user_comment = Comment(comment=form.comment.data,
+                               user_id=current_user.id,
+                               bet_id=bet.id)
 
         db.session.add(user_comment)
         db.session.commit()
 
         user_comment = user_comment.make_dict()
 
-        return jsonify({ 'data': user_comment }), 201
+        return jsonify({'data': user_comment}), 201
 
     else:
         return jsonify({"ERROR": "No comments yet"})
