a2317ca1198edca71bf63cae7dfaccf92463f130
hifilorau <tomrau@gmail.com>
2015-03-21 15:50:08 -0400

    shitty payment solution front-end side

---
+++ charity_bets/static/js/vendor.js
@@ -0,0 +1,23 @@
+angular.module('stripe', []).directive('stripeForm', ['$window',
+function($window) {
+
+  var directive = { restrict: 'A' };
+  directive.link = function(scope, element, attributes) {
+    var form = angular.element(element);
+    form.bind('submit', function() {
+      var button = form.find('button');
+      button.prop('disabled', true);
+      $window.Stripe.createToken(form[0], function() {
+        button.prop('disabled', false);
+        var args = arguments;
+        scope.$apply(function() {
+          scope.$eval(attributes.stripeForm).apply(scope, args);
+        });
+      });
+    });
+  };
+  return directive;
+
+}]);
+
+//# sourceMappingURL=vendor.js.map
