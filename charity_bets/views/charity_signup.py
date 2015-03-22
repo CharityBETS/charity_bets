6000af0fcb3fc6fb47e4c1de4e3c4e033b680298
DanielKN <dknewell1@gmail.com>
2015-03-22 14:14:23 -0400

    cleaned up some of my dirtay code

--- charity_bets/views/charity_signup.py
+++ charity_bets/views/charity_signup.py
@@ -1,5 +1,7 @@
-from flask import session, Blueprint, url_for, request, redirect, flash, render_template, jsonify
-from flask.ext.login import current_user, abort, login_user, logout_user, login_required
+from flask import (session, Blueprint, url_for, request, redirect, flash,
+                   render_template, jsonify)
+from flask.ext.login import (current_user, abort, login_user,
+                             logout_user, login_required)
 from ..models import Charity
 from ..extensions import db
 from flask import current_app as app
@@ -9,46 +11,59 @@ import stripe
 
 charity_signup = Blueprint("charity_signup", __name__)
 
-# might need to take out publishable key here, decide later
+
 @charity_signup.route('/charity_signup')
+
+
 def index():
-  return render_template('charity_signup.html')
+    """Generates the webpage that charities will use to get stripe token"""
+    return render_template('charity_signup.html')
 
 
 @charity_signup.route('/authorize')
 def authorize():
-  site  = 'https://connect.stripe.com' + '/oauth/authorize'
-  params = {
-             'response_type': 'code',
-             'scope': 'read_write',
-             'client_id': app.config.get('CLIENT_ID')
-           }
+    """Rediects user to stripe signup website, which will return a stripe
+    key for charging users on their behalf"""
 
-  # Redirect to Stripe /oauth/authorize endpoint
-  url = site + '?' + urllib.parse.urlencode(params)
-  return redirect(url)
+    site = 'https://connect.stripe.com' + '/oauth/authorize'
+    params = {'response_type': 'code',
+              'scope': 'read_write',
+              'client_id': app.config.get('CLIENT_ID')
+             }
+    url = site + '?' + urllib.parse.urlencode(params)
+    return redirect(url)
+
 
 @charity_signup.route('/oauth/callback')
 def callback():
-  code   = request.args.get('code')
-  data   = {
-             'client_secret': app.config.get('CLIENT_SECRET'),
-             'grant_type': 'authorization_code',
-             'client_id': app.config.get('CLIENT_ID'),
-             'code': code
-           }
+    """Sends application information and requests stripe key for the Stripe Connect
+    user"""
 
-  url = 'https://connect.stripe.com' + '/oauth/token'
-  resp = requests.post(url, params=data)
-  resp = resp.json()
-  token = resp['access_token']
-  stripe.api_key = token
-  email = stripe.Account.retrieve()["email"]
-  charity = Charity(token=token, email=email)
-  db.session.add(charity)
-  db.session.commit()
+    code  = request.args.get('code')
+    data  = {'client_secret': app.config.get('CLIENT_SECRET'),
+              'grant_type': 'authorization_code',
+              'client_id': app.config.get('CLIENT_ID'),
+              'code': code
+             }
+    url = 'https://connect.stripe.com' + '/oauth/token'
+    resp = requests.post(url, params=data)
+    resp = resp.json()
+    token = resp['access_token']
+    stripe.api_key = token
+    email = stripe.Account.retrieve()["email"]
 
-  return render_template('thank_you.html', token=token)
+    # check to see if charity is already in the database
+    duplicate = Charity.query.filter_by(email = email)
+    if duplicate:
+        duplicate.token = token
+        db.session.commit()
+
+    else:
+        charity = Charity(token=token, email=email)
+        db.session.add(charity)
+        db.session.commit()
+
+    return render_template('thank_you.html')
 
 
 @charity_signup.route('/PAYMENT_TEST')
