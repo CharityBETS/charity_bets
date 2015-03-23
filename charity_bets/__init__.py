82f82d77bdd0358283a3829db90de7861b8b4d90
Daniel Newell <dknewell1@gmail.com>
2015-03-23 16:55:31 -0400

    further tries on environ vars

--- charity_bets/__init__.py
+++ charity_bets/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import os
 from . import models
 from .extensions import db, migrate, config, oauth, assets, login_manager, mail
 # from .views.views import coaction
@@ -8,25 +8,20 @@ from .views.users import users
 from .views.bets import bets
 from .views.charities import charities
 from .views.charity_signup import charity_signup
-import logging
-import sys
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(__name__)
 
-    # app.config['DEBUG']
-    # app.config['SECRET_KEY']
-    # app.config['FACEBOOK']
-    # app.config['MAIL_SERVER']
-    # app.config['MAIL_PORT']
-    # app.config['MAIL_USE_TLS']
-    # app.config['MAIL_USE_SSL']
-    # app.config['MAIL_USERNAME']
-    # app.config['MAIL_PASSWORD']
-
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
-    app.logger.setLevel(logging.ERROR)
+    app.config['DEBUG'] = os.environ['DEBUG']
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+    app.config['FACEBOOK'] = os.environ['FACEBOOK']
+    app.config['MAIL_SERVER'] = os.environ['MAIL_SERVER']
+    app.config['MAIL_PORT'] = os.environ['MAIL_PORT']
+    app.config['MAIL_USE_TLS'] = os.environ['MAIL_USE_TLS']
+    app.config['MAIL_USE_SSL'] = os.environ['MAIL_USE_SSL']
+    app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
+    app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
 
     app.register_blueprint(home)
     app.register_blueprint(users)
