9dce1fe111814116d7bb06bfa40e33066f3dde5c
Daniel Newell <dknewell1@gmail.com>
2015-03-24 15:11:30 -0400

    added client id to config

--- charity_bets/__init__.py
+++ charity_bets/__init__.py
@@ -19,6 +19,7 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
     app.config['MAIL_SERVER'] = os.environ['MAIL_SERVER']
     app.config['MAIL_PORT'] = os.environ['MAIL_PORT']
+    app.config['CLIENT_ID'] = os.environ['CLIENT_ID']
     # app.config['MAIL_USE_TLS'] = os.environ['MAIL_USE_TLS']
     app.config['MAIL_USE_SSL'] = os.environ['MAIL_USE_SSL']
     app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
