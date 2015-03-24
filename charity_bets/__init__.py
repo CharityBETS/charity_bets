0d9198cba96960c7ec48332d1c8566b4a814e871
Daniel Newell <dknewell1@gmail.com>
2015-03-24 15:06:36 -0400

    added client_id

--- charity_bets/__init__.py
+++ charity_bets/__init__.py
@@ -25,6 +25,7 @@ def create_app():
     app.config['MAIL_USE_SSL']
     app.config['MAIL_USERNAME']
     app.config['MAIL_PASSWORD']
+    app.config['CLIENT_ID']
 
 
     app.register_blueprint(home)
