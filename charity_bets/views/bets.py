1d9aa04ed1bea720dff3d5621715edfd8cd333ec
Daniel Newell <dknewell1@gmail.com>
2015-03-29 11:09:47 -0400

    preliminary changes to donation

--- charity_bets/views/bets.py
+++ charity_bets/views/bets.py
@@ -453,6 +453,7 @@ def fund_bet(id):
         charity = Charity.query.filter_by(name = bet.charity_creator).first()
         isfunding = bet.challenger
         bet.challenger_money_raised += amount
+        bet.total_money_raised += amount
         db.session.commit()
 
     stripe.api_key = charity.access_token
